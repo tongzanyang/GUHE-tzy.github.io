45bfd3b532c4f80ff6705c62d4f54167d24be67a
故河 <2916812423@qq.com>
2024-12-01 19:46:35 +0800

    Site updated: 2024-12-01 19:46:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/30/hello-world/","2024/11/30/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2024/11/30/P1/","2024/11/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
