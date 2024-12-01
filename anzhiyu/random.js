eba28accaa1b479024eeaeeb32420a5d605201f2
故河 <2916812423@qq.com>
2024-12-01 21:07:32 +0800

    Site updated: 2024-12-01 21:07:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/30/P1/","2024/11/30/hello-world/"];function toRandomPost(){
+var posts=["2024/11/30/P1/","2024/12/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
